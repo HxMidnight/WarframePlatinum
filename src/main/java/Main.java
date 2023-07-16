7292e881505541a28a73bdf490f258cfb9e880ff
HxMidnight <haxemidnight@gmail.com>
2023-07-16 11:57:55 +0700

    If JSON file is not on directory then it will automatically connect to the API and get all warframes in game

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 
+import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class Main {
     public static ArrayList<Warframe> warframeArrayList;
     public static void main(String[] args) {
-
-        new IntroWindow("WarframePlat");
+        if(new File(String.valueOf(Paths.get("warframe.json"))).isFile()) {
+            new IntroWindow("WarframePlat");
+        } else {
+            Data data = new Data();
+            warframeArrayList = data.getWarframeData();
+            new MainFrame("WarframePlat");
+        }
     }
 }
